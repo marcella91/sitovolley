d12a600ae3600620eef5c4728c7b5091ecb40f4a
marcella91 <33283772+marcella91@users.noreply.github.com>
2018-04-18 18:47:48 +0200

    Add files via upload

---
+++ WEB1/nav.php
@@ -0,0 +1,39 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+		<a class="navbar-brand" href="#"> <img height="60px" src="images/mori.jpg"> </a>
+		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+			<span class="navbar-toggler-icon"></span>
+		</button>
+
+		<div class="collapse navbar-collapse" id="navbarSupportedContent">
+			<li class="nav-item dropdown">
+					<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+						Atleti
+					</a>
+					<div class="dropdown-menu" aria-labelledby="navbarDropdown">
+						<a class="dropdown-item" href="#">Iscriviti</a>
+						<a class="dropdown-item" href="#">Ricerca atleta</a>
+						<div class="dropdown-divider"></div>
+					</div>
+				<li class="nav-item dropdown">
+					<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+						Società
+					</a>
+					<div class="dropdown-menu" aria-labelledby="navbarDropdown">
+						<a class="dropdown-item" href="#">Iscriviti</a>
+						<a class="dropdown-item" href="#">Ricerca società</a>
+						<div class="dropdown-divider"></div>
+					</div>
+				<li class="nav-item dropdown">
+					<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+						Allenatori
+					</a>
+					<div class="dropdown-menu" aria-labelledby="navbarDropdown">
+						<a class="dropdown-item" href="#">Iscriviti</a>
+						<a class="dropdown-item" href="#">Ricerca allenatore</a>
+						<div class="dropdown-divider"></div>
+					</div>
+				</li>
+				</li>	
+			</li>
+		</div>
+	</nav>
